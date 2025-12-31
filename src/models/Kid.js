1b315fc1fd1ab278f7f7096a6e17482faf20f22e
siddharaj <gohilsiddharaj13325@gmail.com>
2025-12-31 19:00:29 +0530

    Saving Work

--- src/models/Kid.js
+++ src/models/Kid.js
@@ -1,40 +1,40 @@
-import mongoose from "mongoose";
+  import mongoose from "mongoose";
 
-const GameSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  played: { type: Boolean, default: false },
-  score: { type: Number, default: 0 },
-});
+  const GameSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    played: { type: Boolean, default: false },
+    score: { type: Number, default: 0 },
+  });
 
-const KidSchema = new mongoose.Schema(
-  {
-    firstName: { type: String, required: true, trim: true },
-    lastName: { type: String, required: true, trim: true },
-    age: { type: Number, required: true, min: 1, max: 18 },
-    sabha: {
-      type: String,
-      required: true,
-      enum: [
-        "Sardarkunj",
-        "Akshar Colony",
-        "Vanmalivanka Ni Pole",
-        "Vadikotdi Ni Pole",
-        "Aambalivali Pole",
-        "Gheekanta",
-        "Vadigam",
-        "Shivshakti",
-        "None",
-      ],
+  const KidSchema = new mongoose.Schema(
+    {
+      firstName: { type: String, required: true, trim: true },
+      lastName: { type: String, required: true, trim: true },
+      age: { type: Number, required: true, min: 1, max: 18 },
+      sabha: {
+        type: String,
+        required: true,
+        enum: [
+          "Sardarkunj",
+          "Akshar Colony",
+          "Vanmalivanka Ni Pole",
+          "Vadikotdi Ni Pole",
+          "Aambalivali Pole",
+          "Gheekanta",
+          "Vadigam",
+          "Shivshakti",
+          "None",
+        ],
+      },
+      mobile: { type: String },
+      address: { type: String, default: "" },
+      pictureUrl: { type: String, required: true },
+      totalScore: { type: Number, default: 0 },
+      games: [GameSchema],
     },
-    mobile: { type: String },
-    address: { type: String, default: "" },
-    pictureUrl: { type: String, required: true },
-    totalScore: { type: Number, default: 0 },
-    games: [GameSchema],
-  },
-  {
-    timestamps: true,
-  }
-);
+    {
+      timestamps: true,
+    }
+  );
 
-export default mongoose.models.Kid || mongoose.model("Kid", KidSchema);
+  export default mongoose.models.Kid || mongoose.model("Kid", KidSchema);
