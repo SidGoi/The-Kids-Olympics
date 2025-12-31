53ffd7721636eb0ea36258c2271fbe067dada20c
siddharaj <gohilsiddharaj13325@gmail.com>
2025-12-31 16:44:19 +0530

    done

--- src/app/page.js
+++ src/app/page.js
@@ -4,12 +4,30 @@ import React from "react";
 
 const RootPage = () => {
   return (
-    <div>
-      <Link href={'/registration'}>
-      <Button className={"font-bold"} size="lg">
-        Go to Registration
-      </Button>
-      </Link>
+    <div className="min-h-[80vh] flex items-center justify-center flex-col gap-5">
+      <h1 className="themefont text-4xl font-black">The Kids Olympics</h1>  
+      <div className="grid grid-cols-2 gap-2 px-10">
+        <Link href={"/registration"} className="w-full">
+          <Button className={"w-full font-bold"} size="lg">
+            Go to Registration
+          </Button>
+        </Link>
+        <Link href={"/"} className="w-full">
+          <Button className={"w-full font-bold bg-green-500"} size="lg">
+            View Live Score
+          </Button>
+        </Link>
+        <Link href={"/"} className="w-full">
+          <Button className={"w-full font-bold bg-blue-600"} size="lg">
+            Games Dashboard
+          </Button>
+        </Link>
+        <Link href={"/"} className="w-full">
+          <Button className={"w-full font-bold bg-orange-400"} size="lg">
+            View All Players
+          </Button>
+        </Link>
+      </div>
     </div>
   );
 };
