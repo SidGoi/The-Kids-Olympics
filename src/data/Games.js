f14e4729be01112a5bf6791fdb61d227ba0cc0b4
siddharaj <gohilsiddharaj13325@gmail.com>
2025-12-31 18:50:51 +0530

    Done

--- src/data/Games.js
+++ src/data/Games.js
@@ -1,7 +1,9 @@
-export default initialGames = [
-  { name: "Game 1" },
-  { name: "Game 2" },
-  { name: "Game 3" },
-  { name: "Game 4" },
-  { name: "Game 5" },
+const Games = [
+  { name: "Game 1", played: false, score: 0 },
+  { name: "Game 2", played: false, score: 0 },
+  { name: "Game 3", played: false, score: 0 },
+  { name: "Game 4", played: false, score: 0 },
+  { name: "Game 5", played: false, score: 0 },
 ];
+
+export default Games;
