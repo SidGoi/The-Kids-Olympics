a9e5b508e1c0df49b0a91f62c558ed3312f7a6a2
Hari Pithwa <haripithwa2005@gmail.com>
2025-12-31 19:03:17 +0530

    games.js updated by hari(dynamic)

--- src/data/Games.js
+++ src/data/Games.js
@@ -1,9 +1,9 @@
 const Games = [
-  { name: "Game 1", played: false, score: 0 },
-  { name: "Game 2", played: false, score: 0 },
-  { name: "Game 3", played: false, score: 0 },
-  { name: "Game 4", played: false, score: 0 },
-  { name: "Game 5", played: false, score: 0 },
+  { name: "Ramatiyal Hathi", slug: "ramatiyal-hathi", played: false, score: 0 },
+  { name: "Limbu Chamchi", slug: "limbu-chamchi", played: false, score: 0 },
+  { name: "Kutra nu hadku", slug: "on-the-bone", played: false, score: 0 },
+  { name: "Rope", slug: "rope", played: false, score: 0 },
+  { name: "Bowl", slug: "bowl", played: false, score: 0 },
 ];
 
 export default Games;
