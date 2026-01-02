6f27943d25f74b0a3669c72821dacbe65e5ca8b4
siddharaj <gohilsiddharaj13325@gmail.com>
2026-01-02 09:02:59 +0530

    reset

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,10 @@ import React from "react";
 
 const RootPage = () => {
   return (
-    <div className="min-h-[80vh] flex items-center justify-center flex-col gap-5">
+    <div className="min-h-screen px-20 py-10">
+
+  
+
       <h1 className="themefont text-4xl font-black">The Kids Olympics</h1>
       <div className="grid grid-cols-2 gap-2 px-10">
         <Link href={"/registration"} className="w-full">
